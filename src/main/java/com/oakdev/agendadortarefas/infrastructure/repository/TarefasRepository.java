85ac84bb1cd5748299552d52bab594dd07955a5e
João Carvalho <jaocarvalho.vlad@gmail.com>
2026-01-14 11:56:30 -0300

    alterado service controller e repository para funcionando de requisicoes método PUT

--- src/main/java/com/oakdev/agendadortarefas/infrastructure/repository/TarefasRepository.java
+++ src/main/java/com/oakdev/agendadortarefas/infrastructure/repository/TarefasRepository.java
@@ -1,6 +1,7 @@
 package com.oakdev.agendadortarefas.infrastructure.repository;
 
 import com.oakdev.agendadortarefas.infrastructure.entity.TarefasEntity;
+import com.oakdev.agendadortarefas.infrastructure.enums.StatusNotificacaoEnum;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,7 +12,9 @@ import java.util.List;
 @Repository
 public interface TarefasRepository extends MongoRepository<TarefasEntity, String> {
 
-    List<TarefasEntity> findByDataEventoBetween(LocalDateTime dataInicial, LocalDateTime dataFinal);
+    List<TarefasEntity> findByDataEventoBetweenAndStatusNotificacaoEnum(LocalDateTime dataInicial,
+                                                                        LocalDateTime dataFinal,
+                                                                        StatusNotificacaoEnum status);
 
     List<TarefasEntity> findByEmailUsuario(String email);
 
